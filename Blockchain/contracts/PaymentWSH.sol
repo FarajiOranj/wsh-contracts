097f3155413543390f3e811b672d246a60355d52
Mohammadreza Faraji <farajioranj@gmail.com>
2023-05-11 12:40:34 +0330

    payment module 70%

---
+++ Blockchain/contracts/PaymentWSH.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.19;
+
+
+import "./constants/PaymentConstants.sol";
+import "./storage/PaymentStorage.sol";
+
+contract PaymentWSH is PaymentStorage {
+
+    function chargeVIP(Tariffs selectedPlan) external {
+        // the coin/token transfer code...
+
+        uint[2] storage tap = VIPstat[msg.sender];
+
+        uint256 time = tap[0];
+
+        if(time == LIFETIME) revert ();
+        else if((time == 0 || time < block.timestamp)) {
+            tap[0] = block.timestamp;
+        }
+
+        tap[0] += TARIFFS_DURATIONS[uint256(selectedPlan)];
+        tap[1] += tariffsRate[selectedPlan];
+    }
+}
---
+++ Blockchain/contracts/storage/PaymentStorage.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.19;
+
+abstract contract PaymentStorage {
+
+    enum Tariffs {
+        oneMonth,
+        threeMonths,
+        sixMonths,
+        oneYear
+    }
+
+    uint64 constant ONE_MONTH = 30 days;
+    uint64 constant THREE_MONTHS = 90 days;
+    uint64 constant SIX_MONTHS = 180 days;
+    uint64 constant ONE_YEAR = 365 days;
+    
+    uint64[4] TARIFFS_DURATIONS = [ONE_MONTH, THREE_MONTHS, SIX_MONTHS, ONE_YEAR];
+
+    uint256 lifeTimePrice;
+    mapping (Tariffs => uint256) public tariffsRate;
+    /*
+        Players VIP status:
+            1st index => Has time to start a new game in vip area?
+                (ticket leverage 6-15 & 5 more playable games)
+                Also registers it in unix time stamp.
+            2nd index => Total payments.
+    */
+    mapping(address player => uint256[2] tap) internal VIPstat;
+}
